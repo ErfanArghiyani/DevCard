06ac9ad2d21ba3449a5f03a265284828667a83c0
Erfan <erfan.arghiyani.90@gmail.com>
2024-08-22 18:39:07 +0330

    Remove Privacy

--- DevCard/DevCard/Controllers/HomeController.cs
+++ DevCard/DevCard/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace DevCard.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
